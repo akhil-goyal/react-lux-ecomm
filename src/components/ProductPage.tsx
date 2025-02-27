ca6b7370910ec0e075a87d694132a094d3419215
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-02-27 16:38:26 -0500

    create product page component and initialize state

---
+++ src/components/ProductPage.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  rating: number;
+  images: string[];
+}
+
+const ProductPage = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const navigate = useNavigate();
+
+  const [product, setProduct] = useState<Product | null>(null);
+
+  return <div>ProductPage</div>;
+};
+
+export default ProductPage;
