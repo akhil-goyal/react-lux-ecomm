c5ac1fa0123c7c4a052a3527fea45c6ab26248f3
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-02-27 14:35:19 -0500

    main component: set initial state

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useFilter } from "./FilterContext";
 
 const MainContent = () => {
-  return (
-    <div>MainContent</div>
-  )
-}
+  const { searchQuery, selectedCategory, minPrice, maxPrice, keyword } =
+    useFilter();
 
-export default MainContent
+  const [products, setProducts] = useState<any[]>([]);
+  const [filter, setFilter] = useState("all");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const itemsPerPage = 12;
+
+  return <div>MainContent</div>;
+};
+
+export default MainContent;
