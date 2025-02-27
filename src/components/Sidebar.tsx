9531c4856155490d571fba1a04b5fde5692979c1
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-02-27 13:30:49 -0500

    sidebar: add json req

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
 
+interface Product {
+  category: string;
+}
+
+interface FetchResponse {
+  products: Product[];
+}
+
 const Sidebar = () => {
   const [categories, setCategories] = useState<string[]>([]);
   const [keywords] = useState<string[]>([
@@ -10,6 +18,19 @@ const Sidebar = () => {
     "shoes",
     "short",
   ]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data: FetchResponse = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching product", error);
+      }
+    };
+    fetchCategories();
+  }, []);
 
   return <div>Sidebar</div>;
 };
