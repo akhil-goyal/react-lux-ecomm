18bb7eacfa0b76057b11b94bd367cb0449657c1c
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-02-27 13:42:56 -0500

    sidebar: template for search and min max

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
         const uniqueCategories = Array.from(
           new Set(data.products.map((product) => product.category))
         );
-        console.log(uniqueCategories);
+        setCategories(uniqueCategories);
       } catch (error) {
         console.error("Error fetching product", error);
       }
@@ -35,7 +35,30 @@ const Sidebar = () => {
     fetchCategories();
   }, []);
 
-  return <div>Sidebar</div>;
+  return (
+    <div className="w-64 p-5 h-screen">
+      <h1 className="text-2xl font-bold mb-10 mt-4">Luxury Mance</h1>
+      <section>
+        <input
+          type="text"
+          className="border-2 rounded px-2 sm:mb-0"
+          placeholder="Search Product"
+        />
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="Min"
+          />
+          <input
+            type="text"
+            className="border-2 mr-2 px-5 py-3 mb-3 w-full"
+            placeholder="Max"
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Sidebar;
